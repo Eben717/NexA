234e6525b52c0765a584f9391ed22c803737020c
Eben <kwabenat679@gmail.com>
2024-12-30 16:52:58 +0000

    added data for menuItems in menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,64 @@
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.svg",
+        label: "Home",
+        href: "/",
+        visible: ["admin", "auditor"],
+      },
+      {
+        icon: "/auditor.svg",
+        label: "Auditor",
+        href: "/list/auditors",
+        visible: ["admin"],
+      },
+      {
+        icon: "/calendar.svg",
+        label: "Events",
+        href: "/list/events",
+        visible: ["admin", "auditor",],
+      },
+      {
+        icon: "/message.svg",
+        label: "Messages",
+        href: "/list/messages",
+        visible: ["admin", "auditor"],
+      },
+      {
+        icon: "/announcement.svg",
+        label: "Announcements",
+        href: "/list/announcements",
+        visible: ["admin", "auditors"],
+      },
+    ],
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.svg",
+        label: "Profile",
+        href: "/profile",
+        visible: ["admin", "auditors"],
+      },
+      {
+        icon: "/setting.svg",
+        label: "Settings",
+        href: "/settings",
+        visible: ["admin", "auditors"],
+      },
+      {
+        icon: "/logout.svg",
+        label: "Logout",
+        href: "/logout",
+        visible: ["admin", "auditors"],
+      },
+    ],
+  },
+];
+
 const Menu = () => {
   return (
     <div className=''>Menu</div>
