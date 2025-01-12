ab2e7513238fe2dae93d4a273568754b44d6aa36
Eben <kwabenat679@gmail.com>
2025-01-12 23:23:06 +0000

    styled the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,19 @@ const Navbar = () => {
     </div>
     {/* ICONS AND USERS */}
     <div className="flex items-center gap-6">
-      <div className="bg-white rounded w-5.5 h-5.5 items-center justify-center">
-        <img src="message.svg" alt="" width={20} height={20} />
+      <div className="bg-white rounded w-6 h-6 items-center justify-center cursor-pointer">
+        <img src="message.svg" alt="" width={20} height={20}/>
        </div>
-      <div className="bg-white rounded w-5.5 h-5.5 items-center justify-center">
+      <div className="bg-white rounded-full w-6 h-6 items-center justify-center cursor-pointer relative">
         <img src="announcement.svg" alt="" width={20} height={20} />
+        <div className="absolute -top-3 -right-3.5 w-3.5 h-3.5 flex items-center justify-center bg-purple-500 text-white rounded-full"></div>
        </div>
        <div className="flex flex-col">
         <span className="text-xs leading-3 font-medium"> Ernest Anane</span>
         <span className="text-[10px] text-gray-500 text-right">Admin</span>
        </div>
        <div className="">
-        <img src="profile.svg" alt="" width={36} height={36} className="rounded-full" />
+        <img src="profile.svg" alt="" width={36} height={36} className="rounded-full bg-white cursor-pointer" />
        </div>
       </div>
     </div>
