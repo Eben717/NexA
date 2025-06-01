573917e7b12f40ad057bcf66cbc95f30d56bea8a
Eben <kwabenat679@gmail.com>
2025-06-01 16:38:12 +0000

    imported mongoose and express in server.js

---
+++ nexa/src/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
